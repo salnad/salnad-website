98733bcbaa2af1ce544508379096c31ceaf05503
snad <snad@umich.edu>
2020-03-08 13:31:17 -0400

    updated

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('mainpage.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
